7edbea9fe9a89d9c7d292b779e6cba48e6b6549b
Josue Lapo <josalex001110@gmail.com>
2025-04-23 22:05:27 -0500

    SASF:jLapo:LEVANTAMIENTO_BACKEND

---
+++ PRUEBA_PRACTICA/BACKEND/ms-comp-prueba-Josué-Lapo/src/main/java/ec/sasf/prueba/Josue/Lapo/MsCompPruebaJosueLapoApplication.java
@@ -0,0 +1,13 @@
+package ec.sasf.prueba.Josue.Lapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompPruebaJosueLapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompPruebaJosueLapoApplication.class, args);
+	}
+
+}
---
+++ PRUEBA_PRACTICA/BACKEND/ms-comp-prueba-Josué-Lapo/src/test/java/ec/sasf/prueba/Josue/Lapo/MsCompPruebaJosueLapoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.sasf.prueba.Josue.Lapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompPruebaJosueLapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
